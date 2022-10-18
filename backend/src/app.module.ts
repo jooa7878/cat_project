d6235b67200ceec8f8d65e78820b4d61f9321b46
sanghyeokahn <jooa7878@naver.com>
2022-10-19 00:43:11 +0900

    feat : 회원가입 기능 구현

---
+++ backend/src/app.module.ts
@@ -0,0 +1,30 @@
+import { CatsService } from './cats/cats.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import mongoose from 'mongoose';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    CatsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev';
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
+  }
+}
